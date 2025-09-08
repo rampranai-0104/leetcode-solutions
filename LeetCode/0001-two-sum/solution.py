4da3d340bc7373a209d2372a98d1f9d5d2f9698f
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2025-09-08 13:51:27 +0000

    Sync LeetCode submission - Two Sum - Runtime - 1729 ms (23.11%), Memory - 18.3 MB (100.00%)

--- LeetCode/0001-two-sum/solution.py
+++ LeetCode/0001-two-sum/solution.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
                 if nums[i]+nums[j]==target:
-                    return [i,j]
+                            return [i,j]
+
