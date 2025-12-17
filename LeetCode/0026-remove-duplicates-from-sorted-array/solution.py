157603f7903d10774f78bba123bb7573af5b117d
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2025-12-17 14:48:01 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array - Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

--- LeetCode/0026-remove-duplicates-from-sorted-array/solution.py
+++ LeetCode/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=nums[j]:
+                j+=1
+                nums[j]=nums[i]
         if not nums:
-            return 0
-        i=0
-        for j in range(1,len(nums)):
-            if nums[j]!=nums[i]:
-                i+=1
-                nums[i]=nums[j]
-        return i+1
+            i=0
+        return j+1
