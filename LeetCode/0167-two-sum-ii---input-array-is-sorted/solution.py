c2ea6da08324cf75c92f8b633768d94d6e7d6fcc
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2026-01-02 14:52:41 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted - Runtime - 7 ms (26.57%), Memory - 18.4 MB (100.00%)

---
+++ LeetCode/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n=len(numbers)
+        i=0
+        j=n-1
+        sum=0
+        while i<j:
+            sum=numbers[i]+numbers[j]
+            if sum==target:
+                return i+1,j+1
+            elif sum>target:
+                j-=1
+            elif sum<target:
+                i+=1
+            
