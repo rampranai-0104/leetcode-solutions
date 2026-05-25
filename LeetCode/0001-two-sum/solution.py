a55bc818492a952f8486d34de7fce9ad10cf78f8
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2026-05-25 14:03:27 +0000

    Sync LeetCode submission - Two Sum - Runtime - 1731 ms (22.19%), Memory - 19.7 MB (81.92%)

--- LeetCode/0001-two-sum/solution.py
+++ LeetCode/0001-two-sum/solution.py
@@ -4,4 +4,4 @@ class Solution:
         for i in range(0,n):
             for j in range(i+1,n):
                 if nums[i]+nums[j]==target:
-                    return [i,j]
+                    return i,j
