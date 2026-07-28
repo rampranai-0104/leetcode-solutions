0a5a10f7c0fec38a81b65f123005124f6bc7ce62
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2026-07-28 15:11:23 +0000

    Sync LeetCode submission - Move Zeroes - Runtime - 7 ms (39.20%), Memory - 20.5 MB (62.66%)

---
+++ LeetCode/0283-move-zeroes/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        i=0
+        j=0
+        for i in range(n):
+            if nums[i]!=0:
+                nums[j]=nums[i]
+                j+=1
+        while j<n:
+            nums[j]=0
+            j+=1
