c516fb7666ff80b14c11e81d339e1ade036e056b
DODDI RAM PRANAI TEJA <2400031614@kluniversity.in>
2026-07-29 15:44:00 +0000

    Sync LeetCode submission - 3Sum - Runtime - 653 ms (40.23%), Memory - 22.2 MB (53.93%)

--- LeetCode/0015-3sum/solution.py
+++ LeetCode/0015-3sum/solution.py
@@ -1,27 +1,25 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         n=len(nums)
         nums=sorted(nums)
-        sum=0
-        tr=[]
+        ans=[]
         for i in range(n-2):
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            left=i+1
-            right=n-1
-            while left<right:
-                sum=nums[i]+nums[left]+nums[right]
-                if sum==0:
-                    tr.append([nums[i], nums[left], nums[right]])
-                    left+=1
-                    right-=1
-                    while left<right and nums[left]==nums[left-1]:
-                        left+=1
-                    while left<right and nums[right]==nums[right+1]:
-                        right-=1
-                elif sum<0:
-                    left+=1
-                elif sum>0:
-                    right-=1
-        return tr
-            
+            l=i+1
+            r=n-1
+            while l<r:
+                s=nums[i]+nums[l]+nums[r]
+                if s==0:
+                    ans.append([nums[i],nums[l],nums[r]])
+                    l+=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    r-=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
+                elif s<0:
+                    l+=1
+                else:
+                    r-=1
+        return ans
